c4c0543a24eaa4699bdb1cbabe07edc7b468a22a
Jes Carlskov <jes.carlskov@protonmail.ch>
2026-04-11 22:33:02 +0200

    Clean out .gitignore'd files

--- DropPlayer/Pods/Target Support Files/Pods-DropPlayerTests/Pods-DropPlayerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DropPlayerTests : NSObject
-@end
-@implementation PodsDummy_Pods_DropPlayerTests
-@end
--- DropPlayer/Pods/google-cast-sdk-dynamic-xcframework/README.md
+++
@@ -1,2 +0,0 @@
-# google-cast-sdk-dynamic-xcframework
-(Unofficial) Google Cast SDK Cocoapod - Dynamic XCFramework with Guest Mode - with Apple Silicon support
